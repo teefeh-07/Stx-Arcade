572fe946a12b747f3a4e4b6642dbf699148ec4c0
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:54:49 +0100

    feat: init default network

--- frontend/lib/transactions.ts
+++ frontend/lib/transactions.ts
@@ -1,3 +1,5 @@
 // Transaction Utilities
 import { StacksMocknet, StacksTestnet, StacksMainnet } from '@stacks/network';
 import { AnchorMode, PostConditionMode } from '@stacks/transactions';
+
+const network = new StacksTestnet();
