301d7961a76ee01a75199bcd766821dbb628605d
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:54:30 +0100

    feat: initialize AppConfig

--- frontend/lib/stacks-wallet.ts
+++ frontend/lib/stacks-wallet.ts
@@ -3,3 +3,4 @@ import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 import { Person } from '@stacks/profile';
 
 // Define App Config
+const appConfig = new AppConfig(['store_write', 'publish_data']);
